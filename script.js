f92ead21a20ae1b306e7ff343419d5b405e2eeae
preshu1 <shresthapreshu26@gmail.com>
2025-11-28 21:17:08 +0545

    added scoreboard and built logic

---
+++ script.js
@@ -0,0 +1,24 @@
+let userScore = 0;
+let compScore = 0;
+const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  let randomIndex = Math.floor(Math.random() * 3);
+  return options[randomIndex];
+};
+const playGame = (userChoice) => {
+  console.log("user choice: ", userChoice);
+
+  //generate computer choice
+  const compChoice = genCompChoice();
+  console.log("comp choice ", compChoice);
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    console.log("choice was clicked!", userChoice);
+    playGame(userChoice);
+  });
+});
